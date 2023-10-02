7cbf81403afa1a927a97697630708dfc155955ac
dsx137 <dsx137@users.noreply.github.com>
2023-10-02 17:45:20 +0000

    deploy: 5947ee4685440651dcc2b5c9cbb5f5e9a590eb4a

---
+++ js/colorChangeIcon.js
@@ -0,0 +1,36 @@
+const favicon_dark = '/img/favicon-dark.svg';
+const favicon_light = '/img/favicon-light.svg';
+
+function setFavicon() {
+    const faviconLink = document.querySelector('link[rel="icon"]');
+    if (faviconLink) {
+        const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        faviconLink.href = isDarkMode ? favicon_dark : favicon_light;
+    }
+}
+
+function setLogo() {
+    const logo = document.querySelector('.navbar__logo img');
+    const html = document.querySelector('html');
+    if (logo && html) {
+        if (html.dataset.theme === 'dark') {
+            logo.src = favicon_dark;
+        } else {
+            logo.src = favicon_light;
+        }
+    }
+}
+
+// 如果你希望在颜色模式更改时更新 favicon，你可以添加以下代码
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setFavicon);
+
+window.addEventListener('load', event => {
+    setFavicon();
+    new MutationObserver(setFavicon).observe(document.head, { childList: true });
+
+    // setLogo();
+    // new MutationObserver(setLogo).observe(document.querySelector('html'), { attributes: true });
+});
+
+//即时更新（残疾
+setInterval(setFavicon, 100);
