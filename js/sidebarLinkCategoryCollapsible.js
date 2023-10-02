e0586438d2cb5f2399386cb7c9f2141fc2f4456b
dsx137 <dsx137@users.noreply.github.com>
2023-10-02 01:17:17 +0000

    deploy: 60a6a55e07b54e9f982a5e9748e05e2bb0a251b3

---
+++ js/sidebarLinkCategoryCollapsible.js
@@ -0,0 +1,61 @@
+function handleClickEvent(element, event) {
+    if (element.getAttribute('href').includes(decodeURIComponent(window.location.pathname.split('/').slice(1).join('/').split('?')[0]))) {
+        event.stopPropagation();
+        event.preventDefault();
+        let div = element.parentElement;
+        if (div != null) {
+            let btn = div.querySelector('button');
+            if (btn != null) {
+                btn.click();
+            }
+        }
+    }
+}
+
+const attachedHandlers = new Set();
+
+function handleSidebar(element) {
+    element.querySelectorAll('a.menu__link--sublist:not(.menu__link--sublist-caret)').forEach(
+        function (child) {
+            if (!attachedHandlers.has(child)) {
+                const handler = event => handleClickEvent(child, event);
+                child.addEventListener('click', handler);
+                attachedHandlers.add(child);
+            }
+        }
+    );
+};
+
+window.addEventListener('load', (event) => {
+    const targetNode = document.body;
+
+    handleSidebar(targetNode);
+
+    // 创建一个观察者实例
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            console.log(mutation.target);
+            handleSidebar(mutation.target);
+            // handleSidebar(targetNode);
+        });
+    });
+
+    // 配置观察选项:
+    const config = {
+        attributes: false, // 观察属性变化
+        childList: true, // 观察子节点的变动
+        subtree: true    // 观察所有后代节点的变动
+    };
+
+
+    // 开始观察指定的 DOM 元素
+    if (targetNode) {
+        observer.observe(targetNode, config);
+    } else {
+        console.error('Element not found!');
+    }
+
+    // 之后，您可以停止观察
+    // observer.disconnect();
+    // }
+});
